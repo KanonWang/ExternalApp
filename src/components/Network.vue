54747d43b476069751aebd3d882de61e649190eb
Kanon <kanonwang@outlook.com>
2019-11-05 15:22:34 +0800

    Add pagination

--- src/components/Network.vue
+++ src/components/Network.vue
@@ -1,16 +1,25 @@
 <template>
-    <el-table v-loading="loading" :data="tableData">
-        <el-table-column prop="companyName" label="Company Name" width="180" />
-        <el-table-column prop="country" label="Country" />
-        <el-table-column prop="email" label="Email" />
-        <el-table-column prop="firstName" label="First Name" />
-        <el-table-column prop="lastName" label="Last Name" />
-        <el-table-column prop="connectionType" label="Connection Type" />
-    </el-table>
+    <div>
+        <el-table v-loading="loading" :data="tableData">
+            <el-table-column prop="companyName" label="Company Name" width="180" />
+            <el-table-column prop="country" label="Country" />
+            <el-table-column prop="email" label="Email" />
+            <el-table-column prop="firstName" label="First Name" />
+            <el-table-column prop="lastName" label="Last Name" />
+            <el-table-column prop="connectionType" label="Connection Type" />
+        </el-table>
+        <el-pagination
+                background
+                layout="pager"
+                :current-page="currentPage"
+                :page-count="pages"
+                @current-change="onPageChange"
+        />
+    </div>
 </template>
 
 <script>
-    import { mapState, mapGetters } from 'vuex';
+    import { mapState, mapGetters, mapMutations } from 'vuex';
 
     export default {
         created() {
@@ -21,8 +30,22 @@
                 tableData: 'Network/tableData'
             }),
             ...mapState({
-                loading: state => state.Network.loading
+                loading: state => state.Network.loading,
+                currentPage: state => state.Network.page + 1,
+                pages: state => state.Network.pages
             })
+        },
+        methods: {
+            reload() {
+                this.$store.dispatch('Network/getConnections')
+            },
+            ...mapMutations({
+                setPage: 'Network/setPage'
+            }),
+            onPageChange(currentPage) {
+                this.setPage(currentPage - 1);
+                this.reload();
+            }
         }
     }
 </script>
--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import Vue from 'vue';
 import App from './components/App.vue';
 import store from './store';
-import { Header, Table, TableColumn, Loading } from 'element-ui';
+import { Header, Pagination, Table, TableColumn, Loading } from 'element-ui';
 
 Vue.use(Header);
+Vue.use(Pagination);
 Vue.use(Table);
 Vue.use(TableColumn);
 Vue.use(Loading);
